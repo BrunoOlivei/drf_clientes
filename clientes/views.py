803f4d9a0de88019e0cd714ff0ba49ea3a173b76
Bruno Oliveira <bruoli3@gmail.com>
2023-04-30 21:51:34 -0300

    Autenticação básica

--- clientes/views.py
+++ clientes/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, filters
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
 from django_filters.rest_framework import DjangoFilterBackend # importando o filtro
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
@@ -11,4 +13,6 @@ class ClientesViewSet(viewsets.ModelViewSet):
     ordering_fields = ['nome'] # ordena por nome 
     search_fields = ['nome', 'cpf'] # adicionando o filtro
     filterset_fields = ['ativo'] # adicionando o filtro
+    authentication_classes = [BasicAuthentication] # adicionando autenticação
+    permission_classes = [IsAuthenticated] # adicionando permissão
 
